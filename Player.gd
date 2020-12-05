740c98f740c7a8040e9df0d333f4c36c11672e71
David Ting <dtin4@student.monash.edu>
2020-12-05 11:15:07 +1100

    Added variable jumping

--- Player.gd
+++ Player.gd
@@ -6,6 +6,10 @@ const gravity = 15 # Acceleration of gravity
 const jump_power = -200 # Power of jump
 const floor_direction = Vector2(0,-1) # Direction normal to floor
 
+# Variables for variable jumping
+var jump_count = 0
+var max_jump_count = 10
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -23,6 +27,14 @@ func _physics_process(delta):
 	if Input.is_action_pressed("jump"):
 		if is_on_floor():
 			velocity.y = jump_power
+		elif jump_count < max_jump_count: #If jump is held for longer, increase jump size
+			jump_count += 1
+			velocity.y = jump_power
+	
+	if Input.is_action_just_released("jump") or is_on_ceiling(): # When you release jump, ensure you cannot jump again
+		jump_count = max_jump_count
+	if is_on_floor(): # Whenever on floor, reset jump_count
+		jump_count = 0
 
 	velocity.y += gravity # Add the acceleration of gravity
 	velocity = move_and_slide(velocity, floor_direction) # Do movement
