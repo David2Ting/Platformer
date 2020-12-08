cee693df1bc7a54572ec5ec7b58e8dd51df389c4
David Ting <dtin4@student.monash.edu>
2020-12-08 22:37:33 +1100

    Lesson 11

--- Enemies/Trunk.gd
+++ Enemies/Trunk.gd
@@ -9,19 +9,27 @@ var on_ground = false
 
 var direction = -1 # Handles the direction the enemy is moving in
 var is_alive = true
+var detected_player = false
+var is_shooting = false
+
+
+var bullet = preload("res://Enemies/TrunkBullet.tscn")
 
 func _ready():
 		pass # Replace with function body.
 
 func _physics_process(delta):
 	if is_alive:
-		velocity.x = speed * direction
-		$AnimatedSprite.play("run")
+		if not detected_player:
+			velocity.x = speed * direction
+			$AnimatedSprite.play("run")
+		else:
+			velocity.x = 0
 		if direction == 1:
 			$AnimatedSprite.flip_h = true
 		else:
 			$AnimatedSprite.flip_h = false
-
+		
 		
 		velocity.y += gravity
 		velocity = move_and_slide(velocity, floor_direction)
@@ -31,16 +39,34 @@ func _physics_process(delta):
 			if collision:
 				if collision.collider.name == 'Player':
 					collision.collider.die()
-		
+
 		if is_on_wall():
-			yield(get_tree(), "physics_frame")
 			direction = direction * -1
-			$RayCast2D.position.x = -$RayCast2D.position.x
-			
+			$RayCast2D.position.x *= -1
+			$PlayerDetect.position.x *= -1
+			$PlayerDetect.cast_to.x *= -1
 		if $RayCast2D.is_colliding() == false:
 			direction = direction * - 1
-			$RayCast2D.position.x = -$RayCast2D.position.x
+			$RayCast2D.position.x *= -1
+			$PlayerDetect.position.x *= -1
+			$PlayerDetect.cast_to.x *= -1
+			
+		if $PlayerDetect.is_colliding() and $PlayerDetect.get_collider().name == 'Player':
+			detected_player = true
+			if not is_shooting:
+				is_shooting = true
+				shoot()
+		else:
+			detected_player = false
 
+func shoot():
+	$AnimatedSprite.play("shoot")
+	yield($AnimatedSprite,"animation_finished")
+	var new_bullet = bullet.instance()
+	add_child(new_bullet)
+	new_bullet.position = $PlayerDetect.position
+	new_bullet.direction = direction
+	is_shooting = false
 
 func die():
 	is_alive = false
---
+++ Enemies/TrunkBullet.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var direction = 1
+const speed = 200
+var velocity = Vector2()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func die():
+	queue_free()
+
+func _physics_process(delta):
+	if direction == 1:
+		$Sprite.flip_h = false
+	else:
+		$Sprite.flip_h = true
+
+	velocity.x = speed * direction - get_parent().velocity.x
+	velocity = move_and_slide(velocity)
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		if collision:
+			if collision.collider.name == "Player":
+				collision.collider.die()
+			die()
+	
